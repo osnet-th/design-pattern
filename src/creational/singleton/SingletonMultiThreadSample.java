29c14187462469114182afe8f0fb236a74e771ac
TaeHyung <osnet.th@gmail.com>
2025-09-22 23:30:00 +0900

    싱글턴 패턴 정리 빌더 패턴 추가 필요

---
+++ src/creational/singleton/SingletonMultiThreadSample.java
@@ -0,0 +1,28 @@
+package creational.singleton;
+
+public class SingletonMultiThreadSample {
+
+    private static SingletonMultiThreadSample instance;
+    private String value;
+
+    private SingletonMultiThreadSample(String value) {
+        this.value = value;
+    }
+
+    public static SingletonMultiThreadSample getInstance(String value) {
+        SingletonMultiThreadSample sample = instance;
+        if(sample != null) {
+            return sample;
+        }
+        synchronized (SingletonMultiThreadSample.class) {
+            if (instance == null) {
+                instance = new SingletonMultiThreadSample(value);
+            }
+            return instance;
+        }
+    }
+
+    public String getString() {
+        return "싱글턴 패턴 연습중입니다.";
+    }
+}
